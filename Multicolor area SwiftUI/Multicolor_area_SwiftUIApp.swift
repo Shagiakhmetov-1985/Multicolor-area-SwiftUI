a3f08595d727cd47e8622eae89301439e9be721e
Marat <Shagiakhmetov2499@gmail.com>
2021-06-05 21:46:47 +0300

    Initial Commit

---
+++ Multicolor area SwiftUI/Multicolor_area_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multicolor_area_SwiftUIApp.swift
+//  Multicolor area SwiftUI
+//
+//  Created by Marat Shagiakhmetov on 05.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct Multicolor_area_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
